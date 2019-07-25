55d89e845cbc82c13715b6de79aa8fb240bd0733
yukimasa ikeda <ykms123456789@gmail.com>
2019-07-25 22:20:18 +0900

    Add camera

--- Camera/ViewController.swift
+++ Camera/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func launchCamera(_ sender: UIBarButtonItem) {
+        
+        let camera = UIImagePickerController.SourceType.camera
+        
+        if UIImagePickerController.isSourceTypeAvailable(camera) {
+            let picker = UIImagePickerController()
+            picker.sourceType = camera
+            picker.delegate = self
+            self.present(picker, animated: true)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        self.imageView.image = image
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        self.dismiss(animated: true)
+    }
 
 }
 
